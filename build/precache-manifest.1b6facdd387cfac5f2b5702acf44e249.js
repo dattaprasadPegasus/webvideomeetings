caceeb197bdaac3a6492db44c48b8990bba0f905
atharvPegasus <atharva.pegasua@gmail.com>
2020-12-08 10:27:05 +0530

    done changes

---
+++ build/precache-manifest.1b6facdd387cfac5f2b5702acf44e249.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529abc043a2238985d5f847ec271e229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e391cf6d16728e2b48a6",
+    "url": "/static/css/2.d7267e15.chunk.css"
+  },
+  {
+    "revision": "ce18bc36d7a49d9ddd24",
+    "url": "/static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "e391cf6d16728e2b48a6",
+    "url": "/static/js/2.23643412.chunk.js"
+  },
+  {
+    "revision": "71e9e4dedda4f381ea56e80067d4fca8",
+    "url": "/static/js/2.23643412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce18bc36d7a49d9ddd24",
+    "url": "/static/js/main.0ff74847.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
