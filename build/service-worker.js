67e9ec8a480f1741978bbb956196daf0a401bef8
hrudaynath.gawde <hrudaynath.gawde@travelxp.tv>
2021-01-05 14:11:27 +0530

    merged

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77488b3ed51c3936d06355f0bf7c4289.js"
+  "/precache-manifest.3d69515b51239e750b26abe5e76075de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
